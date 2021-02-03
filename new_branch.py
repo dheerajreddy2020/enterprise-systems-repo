4b554316d2b8da4158f5f49d0819bb2611e3fa19
dheerajreddy2020 <reddycae2012@gmail.com>
2021-02-02 22:29:13 -0500

    commit to test-branch

---
+++ new_branch.py
@@ -0,0 +1,23 @@
+def add(a,b) :
+    '''
+    adding function
+    a and b are the inputs to be given to perform add function
+    example : add(3,2) output is 5
+    '''
+    return a+b
+
+def multiply(a,b):
+    '''
+    multiplication function
+    a and b are the inputs to be given to perform add function
+    example : add(3,2) output is 6
+    '''
+    return a*b
+
+def substract(a,b):
+    '''
+    substraction function
+    a and b are the inputs to be given to perform add function
+    example : add(3,2) output is 1
+    '''
+    return a-b
